d7f218ecbdd0f97ef3a37fe81bd63cbb446a58c1
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-05-28 09:22:36 +0300

    пробная заливка реализации аларма RtData ><= const

---
+++ org.toxsoft.skf.alarms.lib/src/org/toxsoft/skf/alarms/lib/filters/RtGwidOpConstSingleFilterType.java
@@ -0,0 +1,108 @@
+package org.toxsoft.skf.alarms.lib.filters;
+
+import org.toxsoft.core.tsgui.bricks.ctx.*;
+import org.toxsoft.core.tslib.av.*;
+import org.toxsoft.core.tslib.av.impl.*;
+import org.toxsoft.core.tslib.av.math.*;
+import org.toxsoft.core.tslib.av.opset.*;
+import org.toxsoft.core.tslib.bricks.filter.*;
+import org.toxsoft.core.tslib.bricks.strid.coll.*;
+import org.toxsoft.core.tslib.bricks.validator.impl.*;
+import org.toxsoft.core.tslib.gw.gwid.*;
+import org.toxsoft.core.tslib.math.cond.*;
+import org.toxsoft.core.tslib.math.cond.filter.*;
+import org.toxsoft.core.tslib.utils.errors.*;
+import org.toxsoft.uskat.core.*;
+import org.toxsoft.uskat.core.api.rtdserv.*;
+import org.toxsoft.uskat.core.connection.*;
+import org.toxsoft.uskat.core.gui.conn.*;
+
+/**
+ * {@link ITsSingleFilterType} implementation for .
+ *
+ * @author dima
+ */
+public class RtGwidOpConstSingleFilterType
+    extends AbstractTsSingleFilterType {
+
+  /**
+   * Constructor.
+   */
+  public RtGwidOpConstSingleFilterType() {
+    super( RtGwidOpConstSingleCondType.TYPE_ID, IOptionSet.NULL, IStridablesList.EMPTY );
+    setDefs( RtGwidOpConstSingleCondType.OPDEF_RTGWID, //
+        RtGwidOpConstSingleCondType.OPDEF_OPERATION, //
+        RtGwidOpConstSingleCondType.OPDEF_CONST );
+  }
+
+  // ------------------------------------------------------------------------------------
+  // ITsSingleFilterType
+  //
+
+  @Override
+  public ITsFilter<ITsGuiContext> create( ITsSingleCondInfo aCombi ) {
+    TsNullArgumentRtException.checkNull( aCombi );
+    TsIllegalArgumentRtException.checkFalse( aCombi.typeId().equals( id() ) );
+    TsValidationFailedRtException.checkError( validateParams( aCombi.params() ) );
+    return doCreateFilter( aCombi.params() );
+  }
+
+  // ------------------------------------------------------------------------------------
+  // To override/implement
+  //
+
+  /**
+   * Implementation must create the filter.
+   * <p>
+   * Argument is already checked by {@link #validateParams(IOptionSet)}.
+   *
+   * @param aParams {@link IOptionSet} - the filter options {@link ITsSingleCondInfo#params()}
+   * @return {@link ITsFilter} - created filter
+   */
+  @Override
+  protected ITsFilter<ITsGuiContext> doCreateFilter( IOptionSet aParams ) {
+    FilterRtGwidOpConst retVal = new FilterRtGwidOpConst( aParams );
+    return retVal;
+  }
+
+  static class FilterRtGwidOpConst
+      implements ITsFilter<ITsGuiContext> {
+
+    private final Gwid         rtGwid;
+    private final EAvCompareOp operation;
+    private final IAtomicValue constVal;
+
+    /**
+     * Construct filter by param {@link IOptionSet}
+     *
+     * @param aParams - filter params {@link IOptionSet}
+     */
+    public FilterRtGwidOpConst( IOptionSet aParams ) {
+      rtGwid = aParams.getValobj( RtGwidOpConstSingleCondType.OPDEF_RTGWID );
+      operation = aParams.getValobj( RtGwidOpConstSingleCondType.OPDEF_OPERATION );
+      constVal = AvUtils.avInt( aParams.getInt( RtGwidOpConstSingleCondType.OPDEF_CONST ) );
+    }
+
+    @Override
+    public boolean accept( ITsGuiContext aTsGuiContext ) {
+      // get current value of rtGwid
+      IAtomicValue rtData = readRtGwid( aTsGuiContext );
+      IAvComparator c = AvComparatorStrict.INSTANCE;
+      return c.avCompare( rtData, operation, constVal );
+    }
+
+    private IAtomicValue readRtGwid( ITsGuiContext aTsGuiContext ) {
+      ISkConnection srcConn = aTsGuiContext.get( ISkConnectionSupplier.class ).defConn();
+      ISkCoreApi coreApi = srcConn.coreApi();
+      ISkRtdataService dataServ = coreApi.getService( ISkRtdataService.SERVICE_ID );
+      if( dataServ.findReadCurrDataChannel( rtGwid ) == null ) {
+        // создаем канал текущих данных
+        dataServ.createReadCurrDataChannels( new GwidList( rtGwid ) );
+      }
+      ISkReadCurrDataChannel rtdCahannel = dataServ.findReadCurrDataChannel( rtGwid );
+
+      return rtdCahannel.getValue();
+    }
+  }
+
+}
