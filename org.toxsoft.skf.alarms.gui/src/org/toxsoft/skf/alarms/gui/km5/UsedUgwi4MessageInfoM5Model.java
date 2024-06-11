8b10dd17b4de62673811d885153ae05b50b33884
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-06-11 11:08:40 +0300

    just update comments

--- org.toxsoft.skf.alarms.gui/src/org/toxsoft/skf/alarms/gui/km5/UsedUgwi4MessageInfoM5Model.java
+++ org.toxsoft.skf.alarms.gui/src/org/toxsoft/skf/alarms/gui/km5/UsedUgwi4MessageInfoM5Model.java
@@ -12,7 +12,9 @@ import org.toxsoft.core.tsgui.m5.model.impl.*;
 import org.toxsoft.core.tsgui.valed.controls.av.*;
 import org.toxsoft.core.tslib.av.*;
 import org.toxsoft.core.tslib.av.impl.*;
+import org.toxsoft.core.tslib.coll.primtypes.impl.*;
 import org.toxsoft.core.tslib.gw.ugwi.*;
+import org.toxsoft.skf.rri.lib.ugwi.*;
 import org.toxsoft.uskat.core.api.ugwis.kinds.*;
 import org.toxsoft.uskat.core.gui.ugwi.valed.*;
 
@@ -62,8 +64,9 @@ public class UsedUgwi4MessageInfoM5Model
    */
   public M5AttributeFieldDef<IUsedUgwi4MessageInfo> USED_UGWI = new M5AttributeFieldDef<>( FID_USED_UGWI, VALOBJ, //
       TSID_NAME, STR_N_USED_UGWI, //
-      TSID_DESCRIPTION, STR_D_USED_UGWI, // FIXME change to list types
-      ValedUgwiSelectorFactory.OPDEF_SINGLE_UGWI_KIND_ID, avStr( UgwiKindSkRtdata.KIND_ID ), //
+      TSID_DESCRIPTION, STR_D_USED_UGWI, //
+      ValedUgwiSelectorFactory.OPDEF_UGWI_KIND_IDS_LIST,
+      avValobj( new StringArrayList( UgwiKindSkRtdata.KIND_ID, UgwiKindRriAttr.KIND_ID ) ), //
       TSID_KEEPER_ID, Ugwi.KEEPER_ID, //
       OPID_EDITOR_FACTORY_NAME, ValedAvValobjUgwiSelectorTextAndButton.FACTORY_NAME //
   ) {
