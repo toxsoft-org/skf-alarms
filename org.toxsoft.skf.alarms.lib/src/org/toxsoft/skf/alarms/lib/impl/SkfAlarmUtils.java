633c71151cd379a1356f12452aa7e8068ee1d171
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-12-05 12:50:29 +0300

    register RRI editors

--- org.toxsoft.skf.alarms.lib/src/org/toxsoft/skf/alarms/lib/impl/SkfAlarmUtils.java
+++ org.toxsoft.skf.alarms.lib/src/org/toxsoft/skf/alarms/lib/impl/SkfAlarmUtils.java
@@ -4,9 +4,12 @@ import org.toxsoft.core.tslib.math.cond.checker.*;
 import org.toxsoft.core.tslib.utils.valobj.*;
 import org.toxsoft.skf.alarms.lib.*;
 import org.toxsoft.skf.alarms.lib.checkers.*;
+import org.toxsoft.skf.rri.lib.ugwi.*;
+import org.toxsoft.skf.rri.struct.gui.ugwi.*;
 import org.toxsoft.uskat.core.*;
 import org.toxsoft.uskat.core.api.*;
 import org.toxsoft.uskat.core.api.ugwis.*;
+import org.toxsoft.uskat.core.gui.ugwi.gui.*;
 import org.toxsoft.uskat.core.impl.*;
 
 /**
@@ -19,6 +22,7 @@ public class SkfAlarmUtils {
   /**
    * Core handler to register all registered Sk-connection bound {@link ISkUgwiKind} when connection opens.
    */
+  @SuppressWarnings( { "rawtypes", "unchecked" } )
   private static final ISkCoreExternalHandler coreRegistrationHandler = aCoreApi -> {
     ISkAlarmService alarmService = aCoreApi.getService( ISkAlarmService.SERVICE_ID );
     ITsCheckerTopicManager<ISkCoreApi> tm = alarmService.getAlarmCheckersTopicManager();
@@ -26,6 +30,15 @@ public class SkfAlarmUtils {
     tm.registerType( new AlertCheckerRtdataVsRriType() );
     tm.registerType( new AlertCheckerRtdataVsAttrType() );
     tm.registerType( new AlertCheckerRriTypeGtZero() );
+
+    ISkUgwiService uServ = aCoreApi.ugwiService();
+    uServ.registerKind( UgwiKindRriAttr.INSTANCE.createUgwiKind( aCoreApi ) );
+    uServ.registerKind( UgwiKindRriLink.INSTANCE.createUgwiKind( aCoreApi ) );
+    ISkUgwiKind uk;
+    uk = uServ.listKinds().getByKey( UgwiKindRriAttr.KIND_ID );
+    uk.registerHelper( IUgwiKindGuiHelper.class, new UgwiGuiHelperRriAttr( (AbstractSkUgwiKind)uk ) );
+    uk = uServ.listKinds().getByKey( UgwiKindRriLink.KIND_ID );
+    uk.registerHelper( IUgwiKindGuiHelper.class, new UgwiGuiHelperRriLink( (AbstractSkUgwiKind)uk ) );
 
     // FIXME create and register UGWIs
 
