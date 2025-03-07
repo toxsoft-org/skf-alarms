9134409352b6036822c844a44af77c42fff9157c
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2025-03-07 08:55:24 +0300

    remove existing alarms before upload

--- org.toxsoft.skf.alarms.skide/src/org/toxsoft/skf/alarms/skide/ISkidePluginAlarmsSharedResources.java
+++ org.toxsoft.skf.alarms.skide/src/org/toxsoft/skf/alarms/skide/ISkidePluginAlarmsSharedResources.java
@@ -15,6 +15,8 @@ public interface ISkidePluginAlarmsSharedResources {
   String STR_OPDEF_GW_ALARMS_INTERFACE_NAME_D  = Messages.getString( "STR_OPDEF_GW_ALARMS_INTERFACE_NAME_D" );  //$NON-NLS-1$
   String FMT_INFO_JAVA_INTERFACE_WAS_GENERATED = Messages.getString( "FMT_INFO_JAVA_INTERFACE_WAS_GENERATED" ); //$NON-NLS-1$
 
-  String FMT_INFO_ALARMS_UPLOADED = Messages.getString( "FMT_INFO_ALARMS_UPLOADED" ); //$NON-NLS-1$
+  String FMT_INFO_ALARMS_UPLOADED         = Messages.getString( "FMT_INFO_ALARMS_UPLOADED" );         //$NON-NLS-1$
+  String STR_CLEAR_MNEMOS_BEFORE_UPLOAD   = Messages.getString("STR_CLEAR_MNEMOS_BEFORE_UPLOAD"); //$NON-NLS-1$
+  String STR_CLEAR_MNEMOS_BEFORE_UPLOAD_D = Messages.getString("STR_CLEAR_MNEMOS_BEFORE_UPLOAD_D"); //$NON-NLS-1$
 
 }
--- org.toxsoft.skf.alarms.skide/src/org/toxsoft/skf/alarms/skide/tasks/upload/TaskAlarmsUpload.java
+++ org.toxsoft.skf.alarms.skide/src/org/toxsoft/skf/alarms/skide/tasks/upload/TaskAlarmsUpload.java
@@ -1,10 +1,17 @@
 package org.toxsoft.skf.alarms.skide.tasks.upload;
 
+import static org.toxsoft.core.tslib.av.EAtomicType.*;
+import static org.toxsoft.core.tslib.av.impl.AvUtils.*;
+import static org.toxsoft.core.tslib.av.metainfo.IAvMetaConstants.*;
 import static org.toxsoft.core.tslib.bricks.gentask.IGenericTaskConstants.*;
 import static org.toxsoft.skf.alarms.skide.ISkidePluginAlarmsSharedResources.*;
+import static org.toxsoft.skide.core.ISkideCoreConstants.*;
 
+import org.toxsoft.core.tslib.av.impl.*;
+import org.toxsoft.core.tslib.av.metainfo.*;
 import org.toxsoft.core.tslib.bricks.ctx.*;
 import org.toxsoft.core.tslib.bricks.strid.coll.*;
+import org.toxsoft.core.tslib.bricks.strid.coll.impl.*;
 import org.toxsoft.core.tslib.bricks.validator.*;
 import org.toxsoft.core.tslib.utils.*;
 import org.toxsoft.core.tslib.utils.errors.*;
@@ -24,6 +31,14 @@ import org.toxsoft.uskat.core.connection.*;
 public class TaskAlarmsUpload
     extends AbstractSkideUnitTaskSync {
 
+  static final String OPID_CLEAR_ALARMS_BEFORE_UPLOAD = SKIDE_FULL_ID + ".ClearAlarmsBeforeUpload"; //$NON-NLS-1$
+
+  static final IDataDef OPDEF_CLEAR_ALARMS_BEFORE_UPLOAD = DataDef.create( OPID_CLEAR_ALARMS_BEFORE_UPLOAD, BOOLEAN, //
+      TSID_NAME, STR_CLEAR_MNEMOS_BEFORE_UPLOAD, //
+      TSID_DESCRIPTION, STR_CLEAR_MNEMOS_BEFORE_UPLOAD_D, //
+      TSID_DEFAULT_VALUE, AV_TRUE //
+  );
+
   /**
    * Constructor.
    *
@@ -31,7 +46,10 @@ public class TaskAlarmsUpload
    * @throws TsNullArgumentRtException any argument = <code>null</code>
    */
   public TaskAlarmsUpload( AbstractSkideUnit aOwnerUnit ) {
-    super( aOwnerUnit, UploadToServerTaskProcessor.INSTANCE.taskInfo(), IStridablesList.EMPTY );
+    super( aOwnerUnit, UploadToServerTaskProcessor.INSTANCE.taskInfo(), // configuration options
+        new StridablesList<>( //
+            OPDEF_CLEAR_ALARMS_BEFORE_UPLOAD //
+        ) );
   }
 
   // ------------------------------------------------------------------------------------
@@ -49,6 +67,14 @@ public class TaskAlarmsUpload
     ISkAlarmService srcAlarmService = coreApi().getService( ISkAlarmService.SERVICE_ID );
     ISkConnection destConn = UploadToServerTaskProcessor.REFDEF_IN_OPEN_SK_CONN.getRef( aInput );
     ISkAlarmService destAlarmService = destConn.coreApi().getService( ISkAlarmService.SERVICE_ID );
+    // if configured, remove all alarms from server
+    if( OPDEF_CLEAR_ALARMS_BEFORE_UPLOAD.getValue( getCfgOptionValues() ).asBool() ) {
+      IStridablesList<ISkAlarm> alarmsToRemove = destAlarmService.listAlarms();
+      for( String alarmId : alarmsToRemove.ids() ) {
+        destAlarmService.removeAlarm( alarmId );
+      }
+    }
+
     IStridablesList<ISkAlarm> srcAlarmsList = srcAlarmService.listAlarms();
     for( ISkAlarm srcAlarm : srcAlarmsList ) {
       DtoAlarm dto = DtoAlarm.makeAlarm( srcAlarm.id(), coreApi() );
